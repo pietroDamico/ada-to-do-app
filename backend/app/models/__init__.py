192e6e08b39be457110f90884630514e87f10804
pietroDamico <63458524+pietroDamico@users.noreply.github.com>
2026-01-11 17:52:59 +0100

    Revert "feat: bootstrap backend api and db"

--- backend/app/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from app.models.todo import TodoItem, TodoList
-from app.models.user import User
-
-__all__ = ["User", "TodoList", "TodoItem"]
-
--- backend/app/schemas/__init__.py
+++
@@ -1,4 +0,0 @@
-from app.schemas.user import Token, UserCreate, UserLogin, UserRead
-
-__all__ = ["Token", "UserCreate", "UserLogin", "UserRead"]
-
